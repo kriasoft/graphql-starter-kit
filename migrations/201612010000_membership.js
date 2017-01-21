08c2927e984989542bf813a5bdce5d4fff8abe47
Ifemide <ifemiadetayo@outlook.com>
2017-01-21 15:02:21 +0000

    Update 20162010000_membership.js file
    Added comment to migration file on the usage of uuid_generate_v1mc when storing IDs,

--- migrations/201612010000_membership.js
+++ migrations/201612010000_membership.js
@@ -15,7 +15,7 @@ module.exports.up = async (db) => {
   await db.raw('CREATE EXTENSION IF NOT EXISTS "hstore"');
 
   await db.schema.createTable('users', (table) => {
-    table.uuid('id').notNullable().defaultTo(db.raw('uuid_generate_v1mc()')).primary();
+    table.uuid('id').notNullable().defaultTo(db.raw('uuid_generate_v1mc()')).primary(); // using uuid_generate_v1mc() as v1mc is time-based, which is better for keyspace fragmentation on disk for tables.
     table.string('email').unique();
     table.boolean('email_confirmed').notNullable().defaultTo(false);
     table.string('password_hash', 100);
