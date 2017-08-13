84b940f2ff8bf6a32dca837e4996fa2985ca0eba
Konstantin Tarkus <hello@tarkus.me>
2017-08-13 16:00:41 +0300

    Add gzip compression

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@
 import path from 'path';
 import express from 'express';
 import cors from 'cors';
+import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import session from 'express-session';
@@ -61,6 +62,7 @@ app.use(
   }),
 );
 
+app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
