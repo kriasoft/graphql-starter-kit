1776254af8f5518bd8298dbcf431324f00e67b08
Konstantin Tarkus <koistya@gmail.com>
2017-09-05 21:34:27 +0300

    Add permissions example (#64)

---
+++ src/__tests__/Context.js
@@ -0,0 +1,21 @@
+/**
+ * Node.js API Starter Kit (https://reactstarter.com/nodejs)
+ *
+ * Copyright © 2016-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+/* eslint-env jest */
+
+import Context from '../Context';
+
+describe('Context', () => {
+  test('ensureIsAuthenticated()', () => {
+    const ctx1 = new Context({ user: null });
+    const ctx2 = new Context({ user: {} });
+    expect(() => ctx1.ensureIsAuthenticated()).toThrow();
+    expect(() => ctx2.ensureIsAuthenticated()).not.toThrow();
+  });
+});
