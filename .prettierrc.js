a7e2e2aac0b2257e872837392befb6562fd43863
Konstantin Tarkus <hello@tarkus.me>
2020-08-26 19:32:28 +0300

    Add .eslintrc.js to the root (#248)

---
+++ .prettierrc.js
@@ -0,0 +1,28 @@
+/**
+ * Prettier configuration
+ *
+ * @see https://prettier.io/docs/en/configuration
+ * @copyright 2016-present Kriasoft (https://git.io/vMINh)
+ *
+ * @type {import("prettier").RequiredOptions}
+ */
+
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "css",
+  vueIndentScriptAndStyle: false,
+  endOfLine: "lf",
+};
