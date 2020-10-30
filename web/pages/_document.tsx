86e9bb6b600c3e5397044282303986940850beae
Konstantin Tarkus <hello@tarkus.me>
2020-10-30 13:14:45 +0300

    Update dependencies

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -5,7 +5,7 @@
  * @copyright 2016-present Kriasoft (https://git.io/vMINh)
  */
 
-import React from "react";
+import * as React from "react";
 import Document, {
   DocumentContext,
   Head,
--- web/relay/ResetRelayContext.ts
+++ web/relay/ResetRelayContext.ts
@@ -2,7 +2,7 @@
  * @copyright 2016-present Kriasoft (https://git.io/vMINh)
  */
 
-import React from "react";
+import * as React from "react";
 
 export type ResetRelayFn = () => void;
 
