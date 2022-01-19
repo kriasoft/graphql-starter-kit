e3e86abc33d7e38c2dcbcfc563af44bbeaa82f7e
Konstantin Tarkus <hello@tarkus.me>
2022-01-19 22:40:51 +0300

    Add Relay plugin to the top-level Babel config

--- babel.config.js
+++ babel.config.js
@@ -23,6 +23,7 @@ module.exports = function config(api) {
     plugins: [
       "@babel/plugin-proposal-class-properties",
       "@babel/plugin-proposal-object-rest-spread",
+      "babel-plugin-relay",
     ],
 
     ignore: api.env() === "test" ? [] : ["**/__tests__/**", "**/*.test.ts"],
