cb82dc77243c57cc44b184296354d23924bffe1d
Konstantin Tarkus <hello@tarkus.me>
2022-01-21 12:51:16 +0300

    [api] The `Node` interface definitions cleanup (#309)

--- api/types/node.ts
+++ api/types/node.ts
@@ -1,42 +1,27 @@
 /* SPDX-FileCopyrightText: 2016-present Kriasoft <hello@kriasoft.com> */
 /* SPDX-License-Identifier: MIT */
 
-import { GraphQLObjectType } from "graphql";
 import { fromGlobalId, nodeDefinitions } from "graphql-relay";
 import { Context } from "../context";
 
 /**
- * The Node interface.
- * https://relay.dev/graphql/objectidentification.htm
+ * The `Node` interface for global object identification.
+ *
+ * @see https://relay.dev/graphql/objectidentification.htm
  */
-export const { nodeInterface, nodeField, nodesField } = nodeDefinitions(
-  (globalId, ctx: Context) => {
-    const { type, id } = fromGlobalId(globalId);
+export const { nodeInterface, nodeField, nodesField } =
+  nodeDefinitions<Context>(
+    (globalId, ctx) => {
+      const { type, id } = fromGlobalId(globalId);
 
-    switch (type) {
-      case "User":
-        return ctx.userById.load(id);
-      case "Instructor":
-        return ctx.instructorById.load(id);
-      case "Class":
-        return ctx.classById.load(id);
-      case "Team":
-        return ctx.teamById.load(id);
-      case "Session":
-        return ctx.sessionById.load(id);
-      default:
-        return null;
-    }
-  },
-  async (obj): Promise<GraphQLObjectType | null> => {
-    const type = await import(".");
-
-    if (obj.email !== undefined) return type.UserType;
-    if (obj.display_name !== undefined) return type.InstructorType;
-    if (obj.published_at !== undefined) return type.ClassType;
-    if (obj.class_id !== undefined) return type.SessionType;
-    if (obj.creator_id !== undefined) return type.TeamType;
-
-    return null;
-  },
-);
+      switch (type) {
+        case "User":
+          return ctx.userById.load(id);
+        default:
+          return null;
+      }
+    },
+    (value): string | undefined => {
+      if (value.email !== undefined) return "User";
+    },
+  );
