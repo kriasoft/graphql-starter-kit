2862936c38ee5214406b51d8631146a41893d97f
Konstantin Tarkus <hello@tarkus.me>
2022-02-19 14:00:48 +0300

    Fix @babel/preset-env target for Node.js tests

--- babel.config.cjs
+++ babel.config.cjs
@@ -11,6 +11,7 @@
 module.exports = function config(api) {
   const isNodeEnv = api.caller((caller) =>
     [
+      "babel-jest",
       "@babel/register",
       "@babel/cli",
       "@babel/node",
