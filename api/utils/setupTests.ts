b13ce28ffc523cfdbff319b89cd104e8d1b895d0
Konstantin Tarkus <hello@tarkus.me>
2022-03-11 15:43:53 +0300

    [`api`] Add setupTests.ts

---
+++ api/utils/setupTests.ts
@@ -0,0 +1,7 @@
+/* SPDX-FileCopyrightText: 2016-present Kriasoft <hello@kriasoft.com> */
+/* SPDX-License-Identifier: MIT */
+
+import path from "node:path";
+
+// Set the current working directory (CWD)
+process.chdir(path.dirname(__dirname));
