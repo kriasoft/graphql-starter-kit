5338f9d710f97f4e306fab9ae61309cd03d708bb
Konstantin Tarkus <hello@tarkus.me>
2022-06-03 17:22:11 +0300

    refactor: Restore `GOOGLE_APPLICATION_CREDENTIALS` var (#343)

--- api/core/storage.ts
+++ api/core/storage.ts
@@ -4,11 +4,6 @@
 import { Storage } from "@google-cloud/storage";
 import env from "../env";
 
-// Use a service account key (credentials) to simplify local testing
-export const storage = new Storage({
-  projectId: env.GOOGLE_CLOUD_PROJECT,
-  credentials: env.GOOGLE_CLOUD_CREDENTIALS,
-});
-
+export const storage = new Storage();
 export const uploadBucket = storage.bucket(env.UPLOAD_BUCKET);
 export const storageBucket = storage.bucket(env.STORAGE_BUCKET);
