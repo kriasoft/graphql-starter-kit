e98cabd89b5521a67303f321524f5822389bf7e5
Konstantin Tarkus <koistya@gmail.com>
2023-06-02 21:01:16 +0200

    chore(edge): Remove Altair in favor of the latest GraphiQL UI

--- edge/routes/api.ts
+++ edge/routes/api.ts
@@ -1,44 +1,17 @@
 /* SPDX-FileCopyrightText: 2014-present Kriasoft */
 /* SPDX-License-Identifier: MIT */
 
-import * as Altair from "../core/altair";
 import { app } from "../core/app";
 
 // Rewrite requests to a Google Cloud Function serving GraphQL API
 // https://example.com/api => https://api-5oemu2yxnw-uc.a.run.app/api
-export const handler = app.all("/api", async ({ req, env }) => {
-  const url = new URL(req.url);
-  const api = new URL(env.API_ORIGIN);
-
-  if (req.method !== "GET") {
-    url.hostname = api.hostname;
-    return await fetch(url.toString(), req);
-  }
-
-  url.hostname = "cdn.jsdelivr.net";
-  url.pathname = "/npm/altair-static/build/dist/index.html";
-
-  let res = await fetch(url.toString(), req);
-  res = new Response(res.body, res);
-  res.headers.set("Content-Type", "text/html; charset=utf-8");
-  res.headers.set("Cache-Control", "public, max-age=604800, must-revalidate");
-
-  return Altair.transform(res, env);
+app.all("/api", async ({ req, env }) => {
+  const { pathname, search } = new URL(req.url);
+  return await fetch(`${env.API_ORIGIN}${pathname}${search}`, req);
 });
 
-app.all("/api/*", async ({ req }) => {
-  const url = new URL(req.url);
-
-  url.hostname = "cdn.jsdelivr.net";
-  url.pathname = `/npm/altair-static/build/dist${url.pathname.substring(4)}`;
-
-  let res = await fetch(url.toString(), req);
-  res = new Response(res.body, res);
-  res.headers.set("Cache-Control", "public, max-age=604800, must-revalidate");
-  return res;
+// Also, handle all the nested routes
+app.all("/api/*", async ({ req, env }) => {
+  const { pathname, search } = new URL(req.url);
+  return await fetch(`${env.API_ORIGIN}${pathname}${search}`, req);
 });
-
-export type LoginHandler = typeof handler;
-export type LoginResponse = {
-  email: string;
-};
