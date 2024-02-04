ed689a78f2cea26fda931f61e45de2f6225b707d
Konstantin Tarkus <hello@tarkus.me>
2024-02-04 23:05:32 +0100

    feat(infra): Docker registry configuration (#391)

--- infra/core/artifacts.tf
+++ infra/core/artifacts.tf
@@ -1,4 +1,25 @@
-# Google Cloud Storage
+# Cloud Storage for Build Artifacts
+
+# Docker Registry
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/artifact_registry_repository
+resource "google_artifact_registry_repository" "cloud_run" {
+  location      = var.gcp_region
+  repository_id = "cloud-run"
+  description   = "Docker repository for Cloud Run services."
+  format        = "DOCKER"
+
+  cleanup_policies {
+    id     = "cloud-run-cleanup"
+    action = "KEEP"
+
+    most_recent_versions {
+      keep_count = 2
+    }
+  }
+}
+
+# Cloud Storage Bucket
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
 #
 # NOTE: The Google account under which the Terraform Cloud agents run must have
 #       the owner role on the target domain name in order to create Google Cloud
@@ -6,7 +27,6 @@
 #       members in the Google Search Console at the following URL:
 #       https://search.google.com/search-console/welcome?new_domain_name=example.com
 #       https://cloud.google.com/storage/docs/domain-name-verification
-
 resource "google_storage_bucket" "pkg" {
   name          = "pkg.${var.root_level_domain}"
   location      = var.gcp_region
